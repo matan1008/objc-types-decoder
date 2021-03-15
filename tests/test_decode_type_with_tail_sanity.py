a42166b90f246925ad097ddcca051ebdf94d58a6
matan <matan1008@gmail.com>
2021-03-15 08:58:04 +0200

    Add unions support.

--- tests/test_decode_type_with_tail_sanity.py
+++ tests/test_decode_type_with_tail_sanity.py
@@ -6,6 +6,7 @@ from objc_types_decoder.decode import decode_with_tail
 @pytest.mark.parametrize('encoded, decoded, tail', [
     ('labcd', 'long', 'abcd'),
     ('{?=i},hey you', 'struct { int x0; }', ',hey you'),
+    ('(?=i),hey you', 'union { int x0; }', ',hey you'),
     ('[4^f]| Baby just say yes!', 'float * x[4]', '| Baby just say yes!'),
 ])
 def test_with_tail(encoded, decoded, tail):
@@ -15,6 +16,7 @@ def test_with_tail(encoded, decoded, tail):
 @pytest.mark.parametrize('encoded, decoded', [
     ('l', 'long'),
     ('{?=i}', 'struct { int x0; }'),
+    ('(?=i)', 'union { int x0; }'),
     ('[4^f]', 'float * x[4]'),
 ])
 def test_without_tail(encoded, decoded):
